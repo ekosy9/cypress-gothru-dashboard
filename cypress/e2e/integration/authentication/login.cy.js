4b238ce6ed321a7eac014bf6e8827edac4dee533
ekosy9 <ekosyaepulloh9@gmail.com>
2025-01-24 20:58:29 +0700

    update

--- cypress/e2e/integration/authentication/login.cy.js
+++ cypress/e2e/integration/authentication/login.cy.js
@@ -1,8 +1,8 @@
 describe('Feature Login', () => {
   it('Main Function', () => {
     cy.visit('https://app.gothru.co')
-    cy.get('input[placeholder="Enter email"]').type('testergothru@gmail.com') 
-    cy.get('input[placeholder="Enter password"]').type('GoThru@123')         
+    cy.get('input[placeholder="Enter email"]',{ timeout: 60000 }).type('testergothru@gmail.com') 
+    cy.get('input[placeholder="Enter password"]',{ timeout: 60000 }).type('GoThru@123')         
     cy.contains('button', 'Log In').click()    
   })
 })
--- cypress/e2e/integration/starter-page/select-language.cy.js
+++ cypress/e2e/integration/starter-page/select-language.cy.js
@@ -1,16 +1,16 @@
 describe('Feature Select Language', () => {
   beforeEach(() => {
     cy.visit('https://app.gothru.co')
-    cy.get('input[placeholder="Enter email"]').type('testergothru@gmail.com') 
-    cy.get('input[placeholder="Enter password"]').type('GoThru@123')         
-    cy.contains('button', 'Log In').click()    
+    cy.get('input[placeholder="Enter email"]',{ timeout: 60000 }).type('testergothru@gmail.com') 
+    cy.get('input[placeholder="Enter password"]', { timeout: 60000 }).type('GoThru@123')         
+    cy.contains('button', 'Log In', { timeout: 60000 }).click()    
   });
   it('Change Language to Italiano and English', () => {
-    cy.get('img[src*="https://app.gothru.co/images/flag/en.png"]').first().click();
-    cy.contains('button', 'Italiano').click()
-    cy.contains('button', 'Fare domanda a').click()
-    cy.get('img[src*="https://app.gothru.co/images/flag/it.png"]').first().click();
-    cy.contains('button', 'English').click()
-    cy.contains('button', 'Apply').click()
+    cy.get('img[src*="https://app.gothru.co/images/flag/en.png"], { timeout: 60000 }').first().click();
+    cy.contains('button', 'Italiano', { timeout: 60000 }).click()
+    cy.contains('button', 'Fare domanda a', { timeout: 60000 }).click()
+    cy.get('img[src*="https://app.gothru.co/images/flag/it.png"], { timeout: 60000 }').first().click();
+    cy.contains('button', 'English', { timeout: 60000 }).click()
+    cy.contains('button', 'Apply', { timeout: 60000 }).click()
   });
 });
