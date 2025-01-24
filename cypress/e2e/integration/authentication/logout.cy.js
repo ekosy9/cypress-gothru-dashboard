ccb6012e155f6d12baeacb68a8b9c1d99eafc13a
ekosy9 <ekosyaepulloh9@gmail.com>
2025-01-24 21:14:45 +0700

    update timeout

--- cypress/e2e/integration/authentication/logout.cy.js
+++ cypress/e2e/integration/authentication/logout.cy.js
@@ -1,13 +1,13 @@
 describe('Feature Logout', () => {
   beforeEach(() => {
     cy.visit('https://app.gothru.co')
-    cy.get('input[placeholder="Enter email"]',{ timeout: 60000 }).type('testergothru@gmail.com') 
-    cy.get('input[placeholder="Enter password"]',{ timeout: 60000 }).type('GoThru@123')         
+    cy.get('input[placeholder="Enter email"]',{ timeout: 120000 }).type('testergothru@gmail.com') 
+    cy.get('input[placeholder="Enter password"]',{ timeout: 120000 }).type('GoThru@123')         
     cy.contains('button', 'Log In').click()    
   });
   it('should logout successfully and redirect to Homepage', () => {
-    cy.get('button').find('img[alt="avatar"]',{ timeout: 60000 }).click();
-    cy.contains('Log Out',{ timeout: 60000 }).click();
+    cy.get('button').find('img[alt="avatar"]',{ timeout: 120000 }).click();
+    cy.contains('Log Out',{ timeout: 120000 }).click();
     cy.url().should('eq', 'https://app.gothru.co/auth');
   });
 });
--- cypress/e2e/integration/starter-page/select-mode.cy.js
+++ cypress/e2e/integration/starter-page/select-mode.cy.js
@@ -1,12 +1,12 @@
 describe('Feature Select Mode', () => {
   beforeEach(() => {
     cy.visit('https://app.gothru.co')
-    cy.get('input[placeholder="Enter email"]').type('testergothru@gmail.com') 
-    cy.get('input[placeholder="Enter password"]').type('GoThru@123')         
+    cy.get('input[placeholder="Enter email"]',{ timeout: 120000 }).type('testergothru@gmail.com') 
+    cy.get('input[placeholder="Enter password"]',{ timeout: 120000 }).type('GoThru@123')         
     cy.contains('button', 'Log In').click()    
   });
   it('Change mode light to dark', () => {
-    cy.get('button i.fa-solid.fa-moon').first().click();
-    cy.get('button i.fa-solid.fa-sun').first().click();
+    cy.get('button i.fa-solid.fa-moon',{ timeout: 120000 }).first().click();
+    cy.get('button i.fa-solid.fa-sun',{ timeout: 120000 }).first().click();
   });
 });
